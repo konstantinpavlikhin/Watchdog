bf1dd16ba10dd29d58aa06202f07b16c44cec9a4
Konstantin Pavlikhin <k.pavlikhin@gmail.com>
2015-02-23 12:39:28 +0300

    Update the sample app to set a public key to a RegistrationController.

--- Sample/AppDelegate.m
+++ Sample/AppDelegate.m
@@ -12,9 +12,29 @@
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+// Name:
+// John Appleseed
+
+// Serial:
+// GBSQEMIA5K734JXT4ZEECT3MKTYD5MCYWZOXRZJ646R2AWYPN4ZXCFYZWNIHX4336BE5VFTY7VR4VVDQK2KNUARQMS3S3NAUNGPUIQ536RBVJOQUC2SQBIGLVC5LQKV3VCX7D6WTKNCKE3NMGMBCS4CC5DGMBOD6UMS4Q===
+
+- (void) applicationDidFinishLaunching: (NSNotification*) aNotification
 {
   WDGRegistrationController* rc = [WDGRegistrationController sharedRegistrationController];
+  
+  NSMutableString* string = [NSMutableString string];
+  
+  [string appendString: @"-----BEGIN PUBLIC KEY-----\n"];
+  
+  [string appendString: @"MHYwEAYHKoZIzj0CAQYFK4EEACIDYgAECGfTRULbwMBUmw007OTW+VyHi/SnHXX8\n"];
+  
+  [string appendString: @"cSPgyb2gH+sUuQ7NYpay+3BIBHyzxh/x9PcdrHOGFwd0lVqP6y6wzPUMy4rHNO4W\n"];
+  
+  [string appendString: @"UZn1CvVGKuuwNoCDjAo4UqBQbuUwH9Ho"];
+  
+  [string appendString: @"-----END PUBLIC KEY-----"];
+  
+  rc.publicKeyPEM = string;
   
   [rc showRegistrationWindow: self];
 }
